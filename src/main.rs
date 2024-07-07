556af0db4c35201b83d67077721364fd400551f2
Egor Ivanov <rust_lang_lover@proton.me>
2024-07-07 14:24:42 +0300

    Project is uploaded

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use axum::{routing::get, Router};
+use axum_client_ip::{InsecureClientIp, SecureClientIp, SecureClientIpSource};
+use std::net::SocketAddr;
+
+async fn handler(insecure_ip: InsecureClientIp, secure_ip: SecureClientIp) -> String {
+    format!("{insecure_ip:?} {secure_ip:?}")
+}
+
+#[tokio::main]
+async fn main() {
+    async fn handler(insecure_ip: InsecureClientIp, secure_ip: SecureClientIp) -> String {
+        format!("{insecure_ip:?} {secure_ip:?}")
+    }
+
+    let app = Router::new().route("/", get(handler))
+        .layer(SecureClientIpSource::ConnectInfo.into_extension());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(
+        listener,
+        // Don't forget to add `ConnectInfo` if you aren't behind a proxy
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+        .await
+        .unwrap()
+}
